94eab101884b4356e3aad5cad54bcdbdbdc4c8ff
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-28 15:36:24 +0800

    json file data deal

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 // 创建app
 const app = express();
+// 配置用于解析 application/json 格式数据的中间件
+app.use(express.json());
+// 配置用于解析 application/x-www-form-urlencoded 格式数据的中间件（表单数据）
+app.use(express.urlencoded({ extended: true }));
 // GET
-app.get("/hello", (req, res, next) => {
-    console.log("this is the get requested");
-    return res.send("hello");
+app.get("/user/:id", (req, res) => {
+    const userId = req.params.id;
+    res.send(`The user ID from the path parameter is: ${userId}`);
 });
 // PUT
-app.put("/", (req, res, next) => {
-    console.log("this is the get requested");
+app.post("/hello", (req, res, next) => {
+    return res.send(req.body.name);
 });
 // POST
 // DELETE
