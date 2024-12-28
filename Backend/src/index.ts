b3df75cc064bffe203157705a8d6d185789b7cd2
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-28 15:44:31 +0800

    basic folder created

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,25 +3,13 @@ import express from "express";
 // 创建app
 const app = express();
 
+// middlewares
 // 配置用于解析 application/json 格式数据的中间件
 app.use(express.json());
 // 配置用于解析 application/x-www-form-urlencoded 格式数据的中间件（表单数据）
 app.use(express.urlencoded({ extended: true }));
 
-// GET
-app.get("/user/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send(`The user ID from the path parameter is: ${userId}`);
-});
-// PUT
-app.post("/hello", (req, res, next) => {
-  return res.send(req.body.name);
-});
-
-// POST
-
-// DELETE
-
+// connections and listeners
 // 端口监听
 app.listen(3000, () => {
   console.log("listening the port 3000!");
