70b7edef3bd102e9ce6674526d7b4c0449152517
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-28 15:06:28 +0800

    express app creation

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,5 +1,7 @@
 import express from "express";
+// 创建app
 const app = express();
+// 端口监听
 app.listen(3000, () => {
     console.log("listening the port 3000!");
 });
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 
+// 创建app
 const app = express();
 
+// 端口监听
 app.listen(3000, () => {
   console.log("listening the port 3000!");
 });
