316b17c7752a5b09c537299e2427e3dbb1dec667
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-28 15:17:52 +0800

    app.get method postman test

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,21 @@ import express from "express";
 // 创建app
 const app = express();
 
+// GET
+app.get("/hello", (req, res, next) => {
+  console.log("this is the get requested");
+  return res.send("hello");
+});
+
+// PUT
+app.put("/", (req, res, next) => {
+  console.log("this is the get requested");
+});
+
+// POST
+
+// DELETE
+
 // 端口监听
 app.listen(3000, () => {
   console.log("listening the port 3000!");
