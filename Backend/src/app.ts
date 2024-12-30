24fb61eb1eec84c5ecbd533957c46fbe0f02a2e7
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-30 10:03:57 +0800

    database and app.ts creted

---
+++ Backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+// 从 dotenv 库中引入的 config 函数，在执行时会自动查找项目根目录下名为 .env 的文件
+import { config } from "dotenv";
+
+config();
+// 创建app
+const app = express();
+
+// middlewares
+// 配置用于解析 application/json 格式数据的中间件
+app.use(express.json());
+// 配置用于解析 application/x-www-form-urlencoded 格式数据的中间件（表单数据）
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,19 +1,4 @@
-import express from "express";
-// 从 dotenv 库中引入的 config 函数，在执行时会自动查找项目根目录下名为 .env 的文件
-import { config } from "dotenv";
-
-config();
-// 创建app
-const app = express();
-
-// middlewares
-// 配置用于解析 application/json 格式数据的中间件
-app.use(express.json());
-// 配置用于解析 application/x-www-form-urlencoded 格式数据的中间件（表单数据）
-app.use(express.urlencoded({ extended: true }));
-
-// connections and listeners
-// 端口监听
+import app from "./app.js";
 app.listen(3000, () => {
   console.log("listening the port 3000!");
 });
