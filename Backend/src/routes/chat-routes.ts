a8912a8417353497696db727051362114bb381cc
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-30 10:57:58 +0800

    routes router

--- Backend/src/routes/chat-routes.ts
+++ Backend/src/routes/chat-routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
 
 const chatRoutes = Router();
+
 export default chatRoutes;
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import userRoutes from "./user-routes.js";
 import chatRoutes from "./chat-routes.js";
 
-// 主路由创建
+// 主路由创建router
 const appRouter = Router();
 
+// 分路由集合routes
 appRouter.use("/user", userRoutes); //api/v1/user
 appRouter.use("/chats", chatRoutes); //api/v1/chats
 export default appRouter;
