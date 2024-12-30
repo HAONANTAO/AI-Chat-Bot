2f224f4435d0f1e5595e0e0b2d1dd8d450464d86
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-30 11:28:38 +0800

    controller api test

--- Backend/src/controllers/user-controller.ts
+++ Backend/src/controllers/user-controller.ts
@@ -1,3 +1,18 @@
-export const getAllUser = () => {
-  //getall users
+import User from "../models/User.js";
+import { NextFunction, Request, Response } from "express";
+export const getAllUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    //getall users 通过model去数据库找
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ message: "error! not ok!", cause: error.message });
+  }
 };
