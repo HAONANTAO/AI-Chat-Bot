f94947d263599510b5fffb3f9c7a77613ace3586
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-30 10:15:18 +0800

    finish basic connections and server part

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,5 +1,15 @@
 import app from "./app.js";
-app.listen(3000, () => {
-    console.log("listening the port 3000!");
+import { connectedToDatabase } from "./db/connection.js";
+// connections and listeners
+const port = process.env.PORT || 3000;
+connectedToDatabase()
+    .then(() => {
+    // while connected then server works
+    app.listen(port, () => {
+        console.log("connection is good, server works!");
+    });
+})
+    .catch((error) => {
+    console.log("connected failed, server is not open");
 });
 //# sourceMappingURL=index.js.map
