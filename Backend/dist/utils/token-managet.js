1a0911b7a922de8fdc939f8f0b2a29f8fad08417
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-31 10:55:41 +0800

    create token

---
+++ Backend/dist/utils/token-managet.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+export const createToken = (id, email, expiresIn) => {
+    const payload = { id, email };
+    // JWT sign
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+    return token;
+};
+//# sourceMappingURL=token-managet.js.map
