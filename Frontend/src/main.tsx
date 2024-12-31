f241570f40cde6bc0a05c8404f32c7dcda1c68f1
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-31 15:22:53 +0800

    components

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createTheme, ThemeProvider } from "@mui/material";
-import
-
+import { BrowserRouter } from "react-router-dom";
 
 const theme = createTheme({
   typography: {
@@ -14,8 +13,10 @@ const theme = createTheme({
 });
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
