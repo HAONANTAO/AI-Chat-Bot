8f40064a0689114e71adf7092c8d3f225397103a
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2024-12-31 16:02:30 +0800

    public images

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+
+import { AppBar, Toolbar } from "@mui/material";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+      <Toolbar sx={{ display: "flex" }}>
+        
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Header
+export default Header;
