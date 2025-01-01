7f3b7cb8e1286104e54ce0bcfbe0b8e0bdbe4ed2
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-01 20:37:40 +0800

    navigation link customised

--- Frontend/src/components/shared/NavigationLink.tsx
+++ Frontend/src/components/shared/NavigationLink.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-
-
-type Props={
-  to:
-}
-const NavigationLink = () => {
-  return <div>NavigationLink</div>;
+import { Link } from "react-router-dom";
+type Props = {
+  to: string;
+  bg: string;
+  text: string;
+  textColor: string;
+  onClick?: () => Promise<void>;
+};
+const NavigationLink = (Props: Props) => {
+  return (
+    <Link
+      to={Props.to}
+      style={{ background: Props.bg, color: Props.textColor }}>
+     
+      {Props.text}
+    </Link>
+  );
 };
 
 export default NavigationLink;
