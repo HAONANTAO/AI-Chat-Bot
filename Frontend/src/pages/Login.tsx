76641ee09f49431cdb597731b345f0b11c3f9f49
陶浩南 <14639548+haonantao-aaron@user.noreply.gitee.com>
2025-01-01 21:19:40 +0800

    login material UI

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Login = () => {
@@ -6,13 +6,50 @@ const Login = () => {
     <>
       <Box width={"100%"} height={"100%"} display="flex" flex="1">
         {/* 自适应大小 ，网页和移动端 */}
+        {/* image机器人 */}
         <Box
           padding={8}
           mt={8}
           display={{ md: "flex", sm: "none", xs: "none" }}>
           <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
         </Box>
-        <Box></Box>
+
+        {/*  */}
+        <Box
+          display={"flex"}
+          flex={{ xs: 1, md: 0.5 }}
+          justifyContent={"center"}
+          alignContent={"center"}
+          padding={2}
+          ml="auto"
+          mt={16}>
+          {/* 表单 */}
+          <form
+            action=""
+            style={{
+              margin: "auto",
+              padding: "30px",
+              boxShadow: "10px 10px 20px #000",
+              borderRadius: "10px",
+              border: "none ",
+            }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}>
+              {/* 文字 */}
+              <Typography
+                variant="h4"
+                textAlign={"center"}
+                padding="2"
+                fontWeight={"600"}>
+                Login
+              </Typography>
+            </Box>
+          </form>
+        </Box>
       </Box>
     </>
   );
